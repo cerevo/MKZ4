99d878bb8e98f8c4739ee319f08ec6d3d55a6f12
sugimoto <tadashi_sugimoto@cerevo.com>
2016-07-15 12:24:17 +0900

    milkcocoaのhtmlファイル追加

---
+++ custom/milkcocoa_javascript/js_func.js
@@ -0,0 +1,12 @@
+var milkcocoa = new MilkCocoa('yieldimlxufzf.mlkcca.com');
+
+function koshin(){
+	window.alert("Hello!");
+}
+
+function send(command){
+
+	milkcocoa.dataStore('data').push({'com':command});
+}
+
+
